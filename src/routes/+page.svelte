cd9d9f78e0038166d308c6d14a02dde26318667f
karau1 <karau1@bfh.ch>
2023-10-30 11:31:17 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<main>
+    <header>
+        <div>
+            <a href="/">
+                <img src="/logo.svg" alt="Logo" />
+            </a>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </nav>    
+        </div>
+    </header>
+</main>  
